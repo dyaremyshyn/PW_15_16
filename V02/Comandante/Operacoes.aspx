f9c79359dc87a2ac9be5af70ff1b159e2605ddee
JonathanMaga <jonathanmagalhaes18@gmail.com>
2015-12-23 16:29:18 +0000

    operaçoes começadas

--- V02/Comandante/Operacoes.aspx
+++ V02/Comandante/Operacoes.aspx
@@ -5,5 +5,26 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder3" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder4" Runat="Server">
+    <asp:Label ID="Label1" runat="server" Text="Operações Agendadas:"></asp:Label>
+    <asp:DropDownList ID="OperacoesAgendadas" runat="server"></asp:DropDownList>
+    <p></p>
+    <asp:Button ID="NovaOperacao" runat="server" Text="NovaOperacao" OnClick="NovaOperacao_Click" />
+    <asp:Label ID="NomeL" visible="false" runat="server" Text="Nome Da Operação:"></asp:Label>
+    <asp:TextBox ID="NomeDaOperacao" visible="false" runat="server"></asp:TextBox>
+    <p></p>
+    <asp:Label ID="TipL" visible="false" runat="server" Text="Tipo Operação:"></asp:Label>
+    <asp:DropDownList ID="Tipo" visible="false" runat="server"></asp:DropDownList>&nbsp; &nbsp; 
+    <asp:Label ID="LocalL" visible="false" runat="server" Text="Local:"></asp:Label>
+    <asp:TextBox ID="Local" visible="false" runat="server"></asp:TextBox>
+    <p></p>
+    <asp:Calendar ID="Data" visible="false" runat="server"></asp:Calendar>
+    <asp:Label ID="Lhora" visible="false" runat="server" Text="Hora:"></asp:Label>
+    <asp:DropDownList ID="Hora" visible="false" runat="server"></asp:DropDownList>
+    <asp:CheckBoxList ID="Agentes" AutoPostBack="true" visible="false" RepeatColumns="5" runat="server"></asp:CheckBoxList>
+    <asp:CheckBoxList ID="Viaturas" visible="false" RepeatColumns="5" runat="server"></asp:CheckBoxList><p></p>
+    <asp:DropDownList ID="Responsavel" Visible="false" runat="server"></asp:DropDownList>
+    <p></p>
+    <asp:Button ID="Inserir" Visible="false" runat="server" Text="Inserir" />
+    <asp:Button ID="Cancelar" Visible="false" runat="server" Text="Cancelar" OnClick="Cancelar_Click" />
 </asp:Content>
 
--- V02/Comandante/Operacoes.aspx.cs
+++ V02/Comandante/Operacoes.aspx.cs
@@ -7,8 +7,120 @@ using System.Web.UI.WebControls;
 
 public partial class Comandante_Operacoes : System.Web.UI.Page
 {
+    static DateTime time;
+   
     protected void Page_Load(object sender, EventArgs e)
     {
+        int count = 0;
+        ListItem aux;
+        BDRegisto bd = new BDRegisto();
+        if (!IsPostBack)
+        {
+             
+            
 
+
+
+            Data.SelectedDate = DateTime.Today;
+            Hora.DataSource = bd.getHorasEntrada();
+            Hora.DataTextField = "HORA";
+            Hora.DataValueField = "Id";
+            Hora.DataTextFormatString = "{0:HH:mm}";
+            Hora.DataBind();
+            time = Data.SelectedDate;
+        }
+
+
+        if (time != Data.SelectedDate)
+        {
+            count = 0;
+            Agentes.Items.Clear();
+            time = Data.SelectedDate;
+            Viaturas.Items.Clear();
+        }
+
+
+
+        DateTime hora = DateTime.ParseExact(Hora.SelectedItem.Text + ":00", "HH:mm:ss", null);
+        hora = new DateTime(Data.SelectedDate.Year, Data.SelectedDate.Month, Data.SelectedDate.Day, hora.Hour, hora.Minute, 0);
+
+        foreach (ListItem li in Agentes.Items)
+        {
+            if (li.Selected)
+            {
+                count++;
+            }
+        }
+
+
+        if (count == 0)
+        {
+
+            Agentes.DataSource = bd.getAgentesDisponiveis(Data.SelectedDate, hora);
+            Agentes.DataTextField = "NOME";
+            Agentes.DataValueField = "DISTINTIVO";
+            Agentes.DataBind();
+        }
+        else
+        {
+            foreach (ListItem li in Agentes.Items)
+            {
+                if (li.Selected)
+                {
+                    aux = new ListItem(li.Text, li.Value);
+                    aux.Selected = false;
+                    Responsavel.Items.Add(aux);
+                }
+            }
+        }
+        if (Viaturas.Items.Count < 1)
+        {
+            Viaturas.DataSource = bd.getViaturaLivre();
+            Viaturas.DataTextField = "MATRICULO";
+            Viaturas.DataValueField = "COD_VEICULO";
+            Viaturas.DataBind();
+
+        }
+       
+    }
+    protected void NovaOperacao_Click(object sender, EventArgs e)
+    {
+        Label1.Visible = false;
+        OperacoesAgendadas.Visible = false;
+        NovaOperacao.Visible = false;
+        NomeL.Visible = true;
+        NomeDaOperacao.Visible = true;
+        TipL.Visible = true;
+        Tipo.Visible = true;
+        LocalL.Visible = true;
+        Local.Visible = true;
+        Data.Visible = true;
+        Lhora.Visible = true;
+        Hora.Visible = true;
+        Agentes.Visible = true;
+        Viaturas.Visible = true;
+        Inserir.Visible = true;
+        Cancelar.Visible = true;
+        Responsavel.Visible = true;
+    }
+    protected void Cancelar_Click(object sender, EventArgs e)
+    {
+        Label1.Visible = true;
+        OperacoesAgendadas.Visible = true;
+        NovaOperacao.Visible = true;
+        NomeL.Visible =false;
+        NomeDaOperacao.Visible = false;
+        TipL.Visible = false;
+        Tipo.Visible = false;
+        LocalL.Visible = false;
+        Local.Visible = false;
+        Data.Visible = false;
+        Lhora.Visible = false;
+        Hora.Visible = false;
+        Agentes.Visible = false;
+        Viaturas.Visible = false;
+        Inserir.Visible = false;
+        Cancelar.Visible = false;
+        Responsavel.Visible = false;
     }
 }
