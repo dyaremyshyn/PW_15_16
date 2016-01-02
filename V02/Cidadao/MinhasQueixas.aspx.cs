a36959e77bedb4a95743229c121fc9ca8c1550dc
JonathanMaga <jonathanmagalhaes18@gmail.com>
2016-01-02 19:38:33 +0000

    fim

--- V02/Cidadao/MinhasQueixas.aspx.cs
+++ V02/Cidadao/MinhasQueixas.aspx.cs
@@ -9,6 +9,7 @@ using System.Web.UI.WebControls;
 
 public partial class Cidadao_MinhasQueixas : System.Web.UI.Page
 {
+    static string queixaatual;
     protected void Page_Load(object sender, EventArgs e)
     {
         BDRegisto bd = new BDRegisto();
@@ -21,7 +22,7 @@ public partial class Cidadao_MinhasQueixas : System.Web.UI.Page
             Queixas.DataValueField = "COD_QUEIXA";
             Queixas.DataBind();
             Queixas.Items.Insert(0, aux);
-
+            queixaatual = Queixas.SelectedValue;
         }
         else
         {
@@ -53,6 +54,24 @@ public partial class Cidadao_MinhasQueixas : System.Web.UI.Page
                     {
                         Situacao.SelectedIndex = 2;
                     }
+                }
+
+                if (queixaatual != Queixas.SelectedValue)
+                {
+                    Mensagens.DataSource = bd.getMinhaQUeixasMensagens(Queixas.SelectedValue);
+                    Mensagens.DataTextField = "DATA";
+                    Mensagens.DataValueField = "IDMENSAGEM";
+                    Mensagens.DataBind();
+                    Mensagens.Items.Insert(0, aux);
+                    queixaatual = Queixas.SelectedValue;
+                }
+
+                if (Mensagens.SelectedIndex != 0)
+                {
+                    DataTable mensagem = bd.getMensagem(Queixas.SelectedValue);
+                    string msg;
+                    msg = (string)mensagem.Rows[0]["MENSAGEM"];
+                    msg = msg + "/n" + "/n" + "Agente: " + bd.getNomeAgente(((decimal)mensagem.Rows[0]["MENSAGEM"]).ToString());
                 }
             }
             else
